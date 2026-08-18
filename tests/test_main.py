e58f908229e46035563a9bc4300497dec3802bec
ricky6187 <lv421107@gmail.com>
2026-08-18 22:07:26 +0800

    fix: use denpendency override for depend()

--- tests/test_main.py
+++ tests/test_main.py
@@ -4,6 +4,7 @@ from app.main import app
 # replace the db and cloudinary result
 from unittest.mock import patch
 from app import models
+from app import auth_service
 
 client = TestClient(app)
 
@@ -31,9 +32,12 @@ def test_create_order_success(db_session):
     db_session.commit()
     db_session.refresh(test_product)
 
-    # use patch so no need check jwt
-    mock_user = {"username": "testuser", "role": "user"}
-    with patch("app.auth_service.get_current_user_from_token", return_value=mock_user):
+    def mock_get_current_user():
+        return {"username": "testuser", "role": "user"}
+
+    app.dependency_overrides[auth_service.get_current_user_from_token] = mock_get_current_user
+
+    try:
         
         payload = {"product_id": test_product.id, "quantity": 3}
         response = client.post("/api/orders", json=payload)
@@ -43,29 +47,38 @@ def test_create_order_success(db_session):
         db_session.refresh(test_product)
         assert test_product.stock == 7
 
+    finally:
+        # clear so won't affect other test case
+        app.dependency_overrides.clear()
+
 def test_create_product_by_admin(db_session):
-    mock_admin = {"username": "admin_boss", "role": "admin"}
+    def mock_require_admin_role():
+        return {"username": "admin_boss", "role": "admin"}
     
     # intercept cloudinary.uploader.upload with mock_upload so it won't ask cloudinary
-    with patch("app.auth_service.require_admin_role", return_value=mock_admin), \
-         patch("cloudinary.uploader.upload") as mock_upload:
+    app.dependency_overrides[auth_service.require_admin_role] = mock_require_admin_role
+    with patch("cloudinary.uploader.upload") as mock_upload:
         
         # the replaced result
         mock_upload.return_value = {"secure_url": "https://cloudinary.com"}
         
         form_data = {"title": "test case", "description": "test description", "original_price": "500", "flash_price": "299", "stock": "50"}
         file_data = {"photo": ("test.jpg", b"fake image bytes", "image/jpeg")}
-        
-        response = client.post("/api/products", data=form_data, files=file_data)
-        
-        assert response.status_code == 201
-        data = response.json()
-        assert data["product"]["photo_url"] == "https://cloudinary.com"
 
-        product_id = data["product"]["id"]
+        try:
+            response = client.post("/api/products", data=form_data, files=file_data)
+            
+            assert response.status_code == 201
+            data = response.json()
+            assert data["product"]["photo_url"] == "https://cloudinary.com"
 
-        # check the function actually called 
-        mock_upload.assert_called_once()
+            product_id = data["product"]["id"]
+
+            # check the function actually called 
+            mock_upload.assert_called_once()
+
+        finally:
+            app.dependency_overrides.clear()
 
     db_product = db_session.query(models.Product).filter(models.Product.id == product_id).first()
     assert db_product.photo_url == "https://cloudinary.com"
